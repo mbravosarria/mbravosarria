f1181d17b5822e1cc5873e7fd0b9900626bcd8e6
mbravosarria <mbravosarria@gmail.com>
2023-12-05 11:42:28 -0600

    test: :bug: fix react never read error
    add React.createElement() to banner tests

--- src/components/banner/Banner.test.tsx
+++ src/components/banner/Banner.test.tsx
@@ -1,4 +1,5 @@
-// import React from "react";
+// Description: Unit tests for Banner component
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Banner from "./Banner.tsx";
@@ -14,14 +15,14 @@ describe("Banner", () => {
 
   it("renders the banner on small screens", () => {
     window.innerWidth = 320;
-    render(<Banner />);
+    render(React.createElement(Banner));
     const bannerImg = screen.getByAltText("Phone banner");
     expect(bannerImg).toBeInTheDocument();
   });
 
   it("renders the banner on large screens", () => {
     window.innerWidth = 1800;
-    render(<Banner />);
+    render(React.createElement(Banner));
     const bannerImg = screen.getByAltText("Desktop banner");
     expect(bannerImg).toBeInTheDocument();
   });
