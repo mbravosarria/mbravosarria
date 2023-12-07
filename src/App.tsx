7cd5f409627123256475e8dbfacfc2cdcf8c48df
mbravosarria <mbravosarria@gmail.com>
2023-12-07 09:19:40 -0600

    feat: :sparkles: create about section
    create responsive about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
+import AboutMe from "./components/about/AboutMe";
 import Banner from "./components/banner/Banner";
 
 function App() {
   return (
-    <>
+    <div className="root">
       <span className="margin" />
       <Banner />
-    </>
+      <AboutMe />
+    </div>
   );
 }
 
